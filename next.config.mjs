370b0875d3ecc3da0efc63cd500f211f37b2ea0c
Hexvg0n <118133299+Hexvg0n@users.noreply.github.com>
2025-04-26 00:28:41 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -16,27 +16,7 @@ const nextConfig = {
       }
     ]
   },
-  // Dodaj te sekcje
-  trailingSlash: true,
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "X-Forwarded-Proto",
-            value: "https"
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload"
-          }
-        ]
-      }
-    ]
-  },
-  // Jeśli używasz statycznych assetów przez CDN
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://dripez.pl' : '',
+  
 };
 
 export default nextConfig;
